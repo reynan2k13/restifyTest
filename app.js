8427a8bc8b99d77b707967c511c40eb4400e16bc
Reynan Bautista <reynanbautista@kinpo.com.ph>
2017-01-04 15:01:07 +0800

    add CRUD

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ server.listen(port ,ip_addr, function(){
     console.log('%s listening at %s ', server.name , server.url);
 });
 
-server.get('/getProductById', function (req, res, next) {
-    products.findOne({
-        productId: req.params.productId
+
+server.get('/getAllProducts', function (req, res, next) {
+    products.find({
     }, function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'application/json; charset=utf-8'
@@ -30,4 +30,91 @@ server.get('/getProductById', function (req, res, next) {
     });
     return next();
 });
+
+server.get('/getProduct', function (req, res, next) {
+    products.find({
+        name: req.params.name
+    }, function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(data));
+    });
+    return next();
+});
+
+server.post('/addProduct', function (req, res, next) {
+    var product = req.params;
+
+    res.setHeader('Access-Control-Allow-Origin','*');
+    	
+    products.save(product, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    return next();
+});
+
+server.put('/editProductName', function(req, res, next) {
+
+    products.update({
+        name : req.params.name
+    	},
+	    {$set: { name : req.params.newname }
+    }, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    return next();
+
+});
+
+server.put('/updateProduct', function (req, res, next) {
+    // get the existing product
+    products.findOne({
+        name: req.params.name
+    }, function (err, data) {
+        // merge req.params/product with the server/product
+ 
+        var updProd = {}; // updated products 
+        // logic similar to jQuery.extend(); to merge 2 objects.
+        for (var n in data) {
+            updProd[n] = data[n];
+        }
+        for (var n in req.params) {
+            updProd[n] = req.params[n];
+        }
+        products.update({
+            name : req.params.name
+        }, updProd, {
+            multi: false
+        }, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    });
+    return next();
+});
+
+server.del('/delProduct', function (req, res, next) {
+    var product = {};
+    	product.name = req.params.name;
+
+    res.setHeader('Access-Control-Allow-Origin','*');
+    	
+    products.remove(product, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    return next();
+});
+
 
